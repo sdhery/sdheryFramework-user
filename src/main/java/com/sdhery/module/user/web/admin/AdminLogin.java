13ec3fae44f5da32a7087b62a8609affc46f4400
sdhery@gmail.com <herys@163.com>
2013-06-09 17:39:58 +0800

    提交修改

--- src/main/java/com/sdhery/module/user/web/admin/AdminLogin.java
+++ src/main/java/com/sdhery/module/user/web/admin/AdminLogin.java
@@ -1,12 +1,15 @@
 package com.sdhery.module.user.web.admin;
 
 import com.sdhery.module.core.web.BaseController;
+import com.sdhery.module.helper.MessageSourceManager;
 import com.sdhery.module.helper.ServiceManager;
 import com.sdhery.module.user.service.ISysUserService;
 import com.sdhery.module.user.vo.SysUserVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,10 +22,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminLogin extends BaseController {
 
     @RequestMapping(value = "/admin/loginOK")
-    public void loginOk(ModelMap modelMap, SysUserVo sysUserVo) {
+    public void loginOk(ModelMap modelMap, SysUserVo sysUserVo,HttpServletRequest request) {
         try {
             int result = ServiceManager.sysUserService.loginResult(sysUserVo.getLoginId(), sysUserVo.getPassword(), ISysUserService.TARGET_SYSTEM);
-            modelMap.put("result",result);
+            modelMap.put("result", MessageSourceManager.getMessage("login.error." + result, request));
             if(result==ISysUserService.LOGIN_SUCCESSFUL){
                 setSuccess(modelMap);
             }else{
